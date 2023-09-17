aea4328a511b55a8212e3ebfcbefe346edc6fd56
Rithika L <rithika.l@tvsnext.com>
2023-09-18 01:31:26 +0530

    [Rithika]Added

--- AddressBook.js
+++ AddressBook.js
@@ -14,40 +14,40 @@ class Contact {
 
 class AddressBook {
     constructor() {
-        this.contacts = [];}
-    
+        this.contacts = [];
+    }
+
     addNewContact(contact) {
-    this.contacts.push(contact);
-    console.log("Contact Added " + contact.FirstName);}
+        this.contacts.push(contact);
+        console.log("Contact Added " + contact.FirstName);
+    }
 
     displayContactDetails() {
-    this.contacts.forEach(element => {
-        console.log("Full Name: "+element.FirstName + " " + element.LastName);
-    });}
+        this.contacts.forEach(element => {
+            console.log("Full Name: " + element.FirstName + " " + element.LastName);
+        });
+    }
+    EditContact(contact) {
+        validate(contact);
+        this.contacts.forEach(element => {
+            if (contact.FirstName == element.FirstName) {
+                element.LastName = contact.LastName;
+                element.Address = contact.Address;
+                element.City = contact.City;
+                element.State = contact.State;
+                element.Zip = contact.Zip;
+                element.PhoneNumber = contact.PhoneNumber;
+                element.Email = contact.Email;
+                console.log("Edited AddressBook Successfully");
+            }
+        });
+    }
+
 }
-const addressbook=new AddressBook();
-const Contact1 = new Contact(
-    FirstName= "Rithika",
-    LastName= "L",
-    Address= "Madhavaram",
-    City= "Chennai",
-    State= "TamilNadu",
-    Zip ="600051",
-    PhoneNumber= "9789286965",
-    Email= "rithi@gmail.com");
-const Contact2 = new Contact(
-     FirstName = "Riya",
-     LastName = "R",
-     Address = "Bhavan",
-     City = "Pathanamthitta",
-     State = "Kerala",
-     Zip = "123456",
-     PhoneNumber = "1023445678",
-     Email = "riya@gmail.com");
-     
-function validateName(Details) {
-    console.log(Details.FirstName + " " + Details.LastName + " " + Details.Address + " " + Details.City 
-    + " " + Details.State + " " + Details.Zip + " " + Details.PhoneNumber + " " + Details.Email);
+
+function validate(Details) {
+    console.log(Details.FirstName + " " + Details.LastName + " " + Details.Address + " " + Details.City
+        + " " + Details.State + " " + Details.Zip + " " + Details.PhoneNumber + " " + Details.Email);
     if (!/^[A-Z][a-zA-Z]{2,}$/.test(Details.FirstName)) {
         throw new Error("Invalid First Name");
     }
@@ -77,7 +77,47 @@ function validateName(Details) {
     }
 }
 
+const addressbook = new AddressBook();
+const Contact1 = new Contact(
+    FirstName = "Rithika",
+    LastName = "LOgachandran",
+    Address = "Madhavaram",
+    City = "Chennai",
+    State = "TamilNadu",
+    Zip = "600051",
+    PhoneNumber = "9789286965",
+    Email = "rithi@gmail.com");
+const Contact2 = new Contact(
+    FirstName = "Riya",
+    LastName = "Rechi",
+    Address = "Bhavan",
+    City = "Pathanamthitta",
+    State = "Kerala",
+    Zip = "123456",
+    PhoneNumber = "1023445678",
+    Email = "riya@gmail.com");
+validate(Contact1);
+validate(Contact2);
 
-validateName(Contact1);
-validateName(Contact2);
+const Contact3 = new Contact(
+    FirstName = "Shanthi",
+    LastName = "Sekar",
+    Address = "Manali",
+    City = "Chennai",
+    State = "TamilNadu",
+    Zip = "600043",
+    PhoneNumber = "1234567893",
+    Email = "shanthi@gmail.com");
+addressbook.addNewContact(Contact3);
+
+const UpdateContact = new Contact(
+    FirstName = "Rithika",
+    LastName = "Logachandran",
+    Address = "Madhavaram",
+    City = "Chennai",
+    State = "TamilNadu",
+    Zip = "600551",
+    PhoneNumber = "9789286965",
+    Email = "rithika@gmail.com");
+addressbook.EditContact(UpdateContact);
 addressbook.displayContactDetails();
